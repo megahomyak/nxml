9652b86d5109b0ea5310054b8b66f57220266a12
megahomyak <g.megahomyak@gmail.com>
2023-01-02 22:50:55 +0500

    Made some more progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,30 @@
+pub struct Nodes(pub Vec<Node>);
+
+pub struct Tag {
+    pub name: String,
+    pub contents: Nodes,
+}
+
 pub enum Node {
-    Tag { name: String, contents: Vec<Node> },
+    Tag(Tag),
     Text(String),
 }
 
+impl Nodes {
+    pub fn tags(&self) -> impl Iterator<Item = &Tag> {
+        self.0.iter().filter_map(|node| {
+            if let Node::Tag(tag) = node {
+                Some(tag)
+            } else {
+                None
+            }
+        })
+    }
+}
+
 pub enum ParsingError {}
+
+pub struct Parser<'parser>(&'parser str);
 
 pub fn parse(s: &str) -> Result<Vec<Node>, ParsingError> {
     let results = Vec::new();
