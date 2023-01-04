9377be6703d0e521c442b552041d7ec377b512c5
megahomyak <g.megahomyak@gmail.com>
2023-01-05 00:37:19 +0500

    It is working for everything except raw `tag_contents`...

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,54 @@
 use std::io::Write;
 
-use nom::{bytes::complete::{is_not, escaped}, IResult, character::complete::one_of};
+use nom::{
+    branch::alt,
+    bytes::complete::{escaped, is_not, tag},
+    character::complete::one_of,
+    combinator::map,
+    multi::many0,
+    sequence::{delimited, separated_pair},
+    IResult,
+};
 
-fn parser(s: &str) -> IResult<&str, &str> {
-    escaped(is_not("\\[]"), '\\', one_of("\\[]"))(s)
+#[derive(Debug)]
+pub enum Node<'a> {
+    Tag {
+        name: &'a str,
+        contents: Option<Vec<Node<'a>>>,
+    },
+    Text(&'a str),
+}
+
+fn text(s: &str) -> IResult<&str, Node<'_>> {
+    map(escaped(is_not("\\[]"), '\\', one_of("\\[]")), |text| {
+        Node::Text(text)
+    })(s)
+}
+
+fn tag_name(s: &str) -> IResult<&str, &str> {
+    escaped(is_not("\\[]:"), '\\', one_of("\\[]:"))(s)
+}
+
+fn tag_without_contents(s: &str) -> IResult<&str, Node<'_>> {
+    map(delimited(tag("["), tag_name, tag("]")), |name| Node::Tag {
+        name,
+        contents: None,
+    })(s)
+}
+
+fn tag_contents(s: &str) -> IResult<&str, Vec<Node>> {
+    many0(alt((text, tag_without_contents, tag_with_contents)))(s)
+}
+
+fn tag_with_contents(s: &str) -> IResult<&str, Node<'_>> {
+    map(
+        delimited(
+            tag("["),
+            separated_pair(tag_name, tag(":"), tag_contents),
+            tag("]"),
+        ),
+        |(name, contents)| Node::Tag { name, contents: Some(contents) },
+    )(s)
 }
 
 fn main() {
@@ -12,6 +57,6 @@ fn main() {
         print!("> ");
         std::io::stdout().flush().unwrap();
         std::io::stdin().read_line(&mut line).unwrap();
-        println!("{:?}", parser(&line.strip_suffix('\n').unwrap()));
+        println!("{:?}", tag_contents(&line.strip_suffix('\n').unwrap()));
     }
 }
