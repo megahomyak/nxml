43442fe674027317f986ce11fc9e8ed40bd1098a
megahomyak <g.megahomyak@gmail.com>
2023-01-04 22:53:13 +0500

    Trying to do it manually still. Checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,8 @@
-pub struct Nodes {
-    pub contents: Vec<Node>,
-}
-
-impl Nodes {
-    pub fn tags(&self) -> impl Iterator<Item = &Tag> {
-        self.contents.iter().filter_map(|node| {
-            if let Node::Tag(tag) = node {
-                Some(tag)
-            } else {
-                None
-            }
-        })
-    }
-}
-
-pub struct Tag {
-    pub name: String,
-    pub contents: Option<Nodes>,
-}
-
 pub enum Node {
-    Tag(Tag),
+    Tag {
+        name: String,
+        contents: Option<Vec<Node>>,
+    },
     Text(String),
 }
 
@@ -31,116 +13,39 @@ pub enum ParsingErrorKind {
     UnexpectedClosingBracket,
 }
 
-#[derive(Clone)]
-pub struct Position {
-    row_number: usize,
-    column_number: usize,
+pub enum ParsingResult<'a, T> {
+    Ok {
+        rest_of_input: RemainingString<'a>,
+        value: T,
+    },
+    Err {
+        kind: ParsingErrorKind,
+    },
 }
 
-pub struct ParsingError {
-    position: Position,
-    kind: ParsingErrorKind,
+#[derive(Clone, Copy)]
+pub struct RemainingString<'a> {
+    pub contents: &'a str,
 }
 
-pub struct Parser<'parser> {
-    input: &'parser str,
-    position: Position,
-}
-
-pub struct ParsingResult<'pr> {
-    pub node: Node,
-    pub rest: &'pr str,
-}
-
-impl<'parser> Parser<'parser> {
-    pub fn new(input: &'parser str) -> Self {
-        Self {
-            input,
-            position: Position {
-                row_number: 1,
-                column_number: 1,
-            },
-        }
-    }
-
-    fn error_here(&mut self, kind: ParsingErrorKind) -> ParsingError {
-        ParsingError {
-            position: self.position.clone(),
-            kind,
-        }
-    }
-
-    fn parse_node(&mut self) -> Result<Option<Node>, ParsingError> {
-        let iter = self.input.chars().enumerate();
-        if let Some((i, c)) = iter.next() {
-            let opening_parenthesis_position = self.position.clone();
-            if c == '[' {
-            } else if c == ']' {
-                Err(self.error_here(ParsingErrorKind::UnexpectedClosingBracket))
-            }
-            while let Some((i, c)) = iter.next() {
-                if c == '[' || ']' {}
-            }
-        } else {
-            Ok(None)
-        }
+fn take_while<'a>(source: &'a mut RemainingString, checker: impl Fn(char) -> bool) -> &'a str {
+    if let Some(parts_separator) = source.contents.find(|c| !checker(c)) {
+        let (result, new_contents) = source.contents.split_at(parts_separator);
+        source.contents = new_contents;
+        result
+    } else {
+        let old_contents = source.contents;
+        source.contents = "";
+        old_contents
     }
 }
 
-pub fn parse_tag_contents(input: &str, mut position: Position) -> Result<Vec<Node>, ParsingError> {
-    let mut nodes = Vec::new();
-    let mut text = String::new();
-    macro_rules! save_text {
-        () => {{
-            nodes.push(Node::Text(text));
-            text = String::new();
-        }};
-    }
-    let mut next = {
-        let mut iter = input.chars();
-        move || {
-            let c = iter.next();
-            if let Some('\n') = c {
-                position.row_number += 1;
-                position.column_number = 1;
-            }
-            c
-        }
-    };
-    let error_here = |kind| Err(ParsingError { position, kind });
-    while let Some(c) = next() {
-        match c {
-            '\\' => {
-                let Some(c) = next() else {return error_here(ParsingErrorKind::NothingWasEscaped)};
-                match c {
-                    '[' | ']' | '\\' => text.push(c),
-                    _ => return error_here(ParsingErrorKind::UnknownCharacterEscaped),
-                }
-            }
-            '[' => {
-                save_text!();
-                let opening_bracket_position = position;
-                let tag_name = String::new();
-                let inner_nodes = Vec::new();
-                nodes.push();
-                inner_nodes.push();
-            }
-            ']' => return error_here(ParsingErrorKind::UnexpectedClosingBracket),
-            _ => text.push(c),
-        }
-    }
-    save_text!();
-    Ok(nodes)
+pub fn parse_text<'a>(input: RemainingString<'a>) -> ParsingResult<'_, &'a str> {
+    let text = take_while(&mut input, |c| !"\\".contains(c))
 }
 
-pub fn parse(input: &str) -> Result<Nodes, ParsingError> {
-    parse_tag_contents(
-        input,
-        Position {
-            row_number: 1,
-            column_number: 1,
-        },
-    )
+pub fn parse_sequential_nodes(input: RemainingString) -> ParsingResult<'_, Vec<Node>> {
+
 }
 
 #[cfg(test)]
