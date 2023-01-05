183f9cae79498e590d29fd2bbe6a7bb584607172
megahomyak <g.megahomyak@gmail.com>
2023-01-05 16:50:20 +0500

    In the process of generalizing...

--- src/main.rs
+++ src/main.rs
@@ -20,28 +20,28 @@ pub enum Node<'a> {
     Text(&'a str),
 }
 
-fn text(s: &str) -> IResult<&str, Node<'_>> {
+fn text<Input, Error>(s: Input) -> IResult<Input, Node<'_>, Error> {
     map(escaped(is_not("\\[]"), '\\', one_of("\\[]")), |text| {
         Node::Text(text)
     })(s)
 }
 
-fn tag_name(s: &str) -> IResult<&str, &str> {
+fn tag_name<Input, Error>(s: Input) -> IResult<Input, &str, Error> {
     escaped(is_not("\\[]:"), '\\', one_of("\\[]:"))(s)
 }
 
-fn tag_without_contents(s: &str) -> IResult<&str, Node<'_>> {
+fn tag_without_contents<Input, Error>(s: Input) -> IResult<Input, Node<'_>, Error> {
     map(delimited(tag("["), tag_name, tag("]")), |name| Node::Tag {
         name,
         contents: None,
     })(s)
 }
 
-fn tag_contents(s: &str) -> IResult<&str, Vec<Node>> {
+fn tag_contents<Input, Error>(s: Input) -> IResult<Input, Vec<Node>, Error> {
     map(many_till(alt((tag_without_contents, tag_with_contents, text)), eof), |(nodes, _)| nodes)(s)
 }
 
-fn tag_with_contents(s: &str) -> IResult<&str, Node<'_>> {
+fn tag_with_contents<Input, Error: >(s: Input) -> IResult<Input, Node<'_>, Error> {
     map(
         delimited(
             tag("["),
@@ -61,6 +61,6 @@ fn main() {
         print!("> ");
         std::io::stdout().flush().unwrap();
         std::io::stdin().read_line(&mut line).unwrap();
-        println!("{:?}", tag_contents(line.strip_suffix('\n').unwrap()));
+        println!("{:?}", tag_contents::<&str, nom::error::VerboseError<&str>>(line.strip_suffix('\n').unwrap()));
     }
 }
