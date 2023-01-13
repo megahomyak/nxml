95347c98cc263f72d5752abafdb69f16eb833c82
megahomyak <g.megahomyak@gmail.com>
2023-01-13 15:42:48 +0500

    Moved to `parco`

--- src/lib.rs
+++ src/lib.rs
@@ -5,258 +5,159 @@ pub enum Node {
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Rest<T>(pub T);
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub struct Position {
-    pub column: usize,
-    pub row: usize,
-}
-
-#[derive(Debug, PartialEq, Eq)]
 pub enum Error {
     EscapeAtTheEndOfInput,
-    UnknownCharacterEscaped { pos: Position },
-    UnclosedBracket { pos: Position },
-    UnexpectedClosingBracket { pos: Position },
+    UnknownCharacterEscaped { pos: parco::Position },
+    UnclosedBracket { pos: parco::Position },
+    UnexpectedClosingBracket { pos: parco::Position },
 }
 
-pub fn parse_one_node(input: &str) -> Result<Option<(Node, Rest<(&str, Position)>)>, Error> {
-    match inner::node::parse(input.into()) {
-        inner::ParsingResult::Ok((node, rest)) => Ok(Some((node, Rest((rest.0.s, rest.0.pos))))),
-        inner::ParsingResult::Err => Ok(None),
-        inner::ParsingResult::Fatal(e) => Err(e.into()),
+pub fn parse_one_node(
+    input: &str,
+) -> Result<Option<(Node, parco::Rest<(&str, parco::Position)>)>, Error> {
+    match node::parse(input.into()) {
+        ParsingResult::Ok((node, rest)) => {
+            Ok(Some((node, parco::Rest((rest.0.src(), rest.0.pos())))))
+        }
+        ParsingResult::Err => Ok(None),
+        ParsingResult::Fatal(e) => Err(e.into()),
     }
 }
 
 pub fn parse_sequential_nodes(input: &str) -> Result<Vec<Node>, Error> {
-    match inner::sequence_of_nodes::parse(input.into()) {
-        inner::ParsingResult::Ok((nodes, rest)) => {
-            if rest.0.s.is_empty() {
+    match sequence_of_nodes::parse(input.into()) {
+        ParsingResult::Ok((nodes, rest)) => {
+            if rest.0.src().is_empty() {
                 Ok(nodes)
             } else {
-                Err(Error::UnexpectedClosingBracket { pos: rest.0.pos })
+                Err(Error::UnexpectedClosingBracket { pos: rest.0.pos() })
             }
         }
-        inner::ParsingResult::Err => Ok(vec![]),
-        inner::ParsingResult::Fatal(e) => Err(match e {
-            inner::FatalError::UnclosedBracket { pos } => Error::UnclosedBracket { pos },
-            inner::FatalError::EscapeAtTheEndOfInput => Error::EscapeAtTheEndOfInput,
-            inner::FatalError::UnknownCharacterEscaped { pos } => {
-                Error::UnknownCharacterEscaped { pos }
-            }
+        ParsingResult::Err => Ok(vec![]),
+        ParsingResult::Fatal(e) => Err(match e {
+            FatalError::UnclosedBracket { pos } => Error::UnclosedBracket { pos },
+            FatalError::EscapeAtTheEndOfInput => Error::EscapeAtTheEndOfInput,
+            FatalError::UnknownCharacterEscaped { pos } => Error::UnknownCharacterEscaped { pos },
         }),
     }
 }
 
-mod inner {
+type ParsingResult<'s, T> = parco::Result<T, parco::PositionedString<'s>, FatalError>;
+
+enum FatalError {
+    EscapeAtTheEndOfInput,
+    UnknownCharacterEscaped { pos: parco::Position },
+    UnclosedBracket { pos: parco::Position },
+}
+
+impl From<FatalError> for Error {
+    fn from(e: FatalError) -> Self {
+        match e {
+            FatalError::UnclosedBracket { pos } => Self::UnclosedBracket { pos },
+            FatalError::EscapeAtTheEndOfInput => Self::EscapeAtTheEndOfInput,
+            FatalError::UnknownCharacterEscaped { pos } => Self::UnknownCharacterEscaped { pos },
+        }
+    }
+}
+
+mod text_character {
     use super::*;
 
-    #[derive(Clone, Copy)]
-    pub struct Input<'a> {
-        pub s: &'a str,
-        pub pos: Position,
+    pub enum Character {
+        VerticalBar,
+        Other(char),
     }
 
-    impl<'a> From<&'a str> for Input<'a> {
-        fn from(s: &'a str) -> Self {
-            Self {
-                s,
-                pos: Position { column: 1, row: 1 },
-            }
-        }
-    }
-
-    pub enum FatalError {
-        EscapeAtTheEndOfInput,
-        UnknownCharacterEscaped { pos: Position },
-        UnclosedBracket { pos: Position },
-    }
-
-    impl From<FatalError> for Error {
-        fn from(e: FatalError) -> Self {
-            match e {
-                inner::FatalError::UnclosedBracket { pos } => Self::UnclosedBracket { pos },
-                inner::FatalError::EscapeAtTheEndOfInput => Self::EscapeAtTheEndOfInput,
-                inner::FatalError::UnknownCharacterEscaped { pos } => {
-                    Self::UnknownCharacterEscaped { pos }
-                }
-            }
-        }
-    }
-
-    pub enum ParsingResult<'a, T> {
-        Ok((T, Rest<Input<'a>>)),
-        Err,
-        Fatal(FatalError),
-    }
-
-    use ParsingResult as PR;
-
-    impl<'a, T> ParsingResult<'a, T> {
-        pub fn and<O>(
-            self,
-            f: impl FnOnce((T, Rest<Input<'a>>)) -> ParsingResult<'a, O>,
-        ) -> ParsingResult<'a, O> {
-            match self {
-                Self::Ok((value, rest)) => f((value, rest)),
-                Self::Err => ParsingResult::Err,
-                Self::Fatal(e) => ParsingResult::Fatal(e),
-            }
-        }
-
-        pub fn or(self, f: impl FnOnce() -> Self) -> Self {
-            if let Self::Err = self {
-                f()
-            } else {
-                self
-            }
-        }
-
-        pub fn map<O>(
-            self,
-            f: impl FnOnce(T) -> O,
-        ) -> ParsingResult<'a, O> {
-            match self {
-                Self::Ok((value, rest)) => ParsingResult::Ok((f(value), rest)),
-                Self::Err => ParsingResult::Err,
-                Self::Fatal(fatal) => ParsingResult::Fatal(fatal),
-            }
-        }
-    }
-
-    fn first_character(input: Input<'_>) -> PR<'_, char> {
-        let mut chars = input.s.chars();
-        chars.next().map_or(PR::Err, |c| {
-            PR::Ok((
-                c,
-                Rest(Input {
-                    s: chars.as_str(),
-                    pos: if c == '\n' {
-                        Position {
-                            row: input.pos.row + 1,
-                            column: 1,
-                        }
+    pub(crate) fn parse(input: parco::PositionedString) -> ParsingResult<Character> {
+        parco::one_part(input).and(|(c, input)| match c {
+            '[' | ']' => parco::Result::Err,
+            '\\' => parco::one_part(input.0)
+                .and(|(c, rest)| {
+                    if "\\|[]".contains(c) {
+                        parco::Result::Ok((Character::Other(c), rest))
                     } else {
-                        Position {
-                            row: input.pos.row,
-                            column: input.pos.column + 1,
-                        }
-                    },
-                }),
-            ))
-        })
-    }
-
-    fn matches(input: Input<'_>, sample: char) -> PR<'_, char> {
-        first_character(input).and(|(c, input)| {
-            if c == sample {
-                PR::Ok((c, input))
-            } else {
-                PR::Err
-            }
-        })
-    }
-
-    mod text_character {
-        use super::*;
-
-        pub enum Character {
-            VerticalBar,
-            Other(char),
-        }
-
-        pub fn parse(input: Input<'_>) -> PR<'_, Character> {
-            first_character(input).and(|(c, input)| match c {
-                '[' | ']' => PR::Err,
-                '\\' => first_character(input.0)
-                    .and(|(c, rest)| {
-                        if "\\|[]".contains(c) {
-                            PR::Ok((Character::Other(c), rest))
-                        } else {
-                            PR::Fatal(FatalError::UnknownCharacterEscaped { pos: input.0.pos })
-                        }
-                    })
-                    .or(|| PR::Fatal(FatalError::EscapeAtTheEndOfInput)),
-                '|' => PR::Ok((Character::VerticalBar, input)),
-                _ => PR::Ok((Character::Other(c), input)),
-            })
-        }
-    }
-
-    mod text {
-        use super::*;
-
-        pub fn parse(mut input: Input<'_>) -> PR<'_, String> {
-            let mut result = String::new();
-            loop {
-                match text_character::parse(input) {
-                    PR::Ok((c, rest)) => {
-                        input = rest.0;
-                        match c {
-                            text_character::Character::VerticalBar => {
-                                result.shrink_to_fit();
-                                return PR::Ok((result, Rest(input)));
-                            }
-                            text_character::Character::Other(c) => {
-                                result.push(c);
-                            }
-                        }
+                        parco::Result::Fatal(FatalError::UnknownCharacterEscaped {
+                            pos: input.0.pos(),
+                        })
                     }
-                    PR::Err => {
-                        if result.is_empty() {
-                            return PR::Err;
-                        }
-                        result.shrink_to_fit();
-                        return PR::Ok((result, Rest(input)));
-                    }
-                    PR::Fatal(e) => return PR::Fatal(e),
-                }
-            }
-        }
-    }
-
-    pub mod sequence_of_nodes {
-        use super::*;
-
-        pub fn parse(mut input: Input<'_>) -> PR<'_, Vec<Node>> {
-            let mut nodes = Vec::new();
-            loop {
-                match node::parse(input) {
-                    PR::Err => {
-                        nodes.shrink_to_fit();
-                        return PR::Ok((nodes, Rest(input)));
-                    }
-                    PR::Ok((node, rest)) => {
-                        input = rest.0;
-                        nodes.push(node);
-                    }
-                    PR::Fatal(e) => return PR::Fatal(e),
-                }
-            }
-        }
-    }
-
-    pub mod node {
-        use super::*;
-
-        pub fn parse(input: Input<'_>) -> PR<'_, Node> {
-            text::parse(input)
-                .map(|text| Node::Text(text))
-                .or(|| {
-                    matches(input, '[').and(|(_c, rest)| {
-                        matches(rest.0, ']')
-                            .map(|_c| Node::Sequence(vec![]))
-                            .or(|| {
-                                sequence_of_nodes::parse(rest.0).and(|(nodes, input)| {
-                                    matches(input.0, ']')
-                                        .map(|_c| Node::Sequence(nodes))
-                                })
-                            })
-                            .or(|| PR::Fatal(FatalError::UnclosedBracket { pos: input.pos }))
-                    })
                 })
+                .or(|| parco::Result::Fatal(FatalError::EscapeAtTheEndOfInput)),
+            '|' => parco::Result::Ok((Character::VerticalBar, input)),
+            _ => parco::Result::Ok((Character::Other(c), input)),
+        })
+    }
+}
+
+mod text {
+    use super::*;
+
+    pub(crate) fn parse(mut input: parco::PositionedString) -> ParsingResult<String> {
+        let mut result = String::new();
+        loop {
+            match text_character::parse(input) {
+                parco::Result::Ok((c, rest)) => {
+                    input = rest.0;
+                    match c {
+                        text_character::Character::VerticalBar => {
+                            result.shrink_to_fit();
+                            return parco::Result::Ok((result, parco::Rest(input)));
+                        }
+                        text_character::Character::Other(c) => {
+                            result.push(c);
+                        }
+                    }
+                }
+                parco::Result::Err => {
+                    if result.is_empty() {
+                        return parco::Result::Err;
+                    }
+                    result.shrink_to_fit();
+                    return parco::Result::Ok((result, parco::Rest(input)));
+                }
+                parco::Result::Fatal(e) => return parco::Result::Fatal(e),
+            }
         }
+    }
+}
+
+pub mod sequence_of_nodes {
+    use super::*;
+
+    pub(crate) fn parse(mut input: parco::PositionedString) -> ParsingResult<Vec<Node>> {
+        let mut nodes = Vec::new();
+        loop {
+            match node::parse(input) {
+                parco::Result::Err => {
+                    nodes.shrink_to_fit();
+                    return parco::Result::Ok((nodes, parco::Rest(input)));
+                }
+                parco::Result::Ok((node, rest)) => {
+                    input = rest.0;
+                    nodes.push(node);
+                }
+                parco::Result::Fatal(e) => return parco::Result::Fatal(e),
+            }
+        }
+    }
+}
+
+pub mod node {
+    use super::*;
+
+    pub(crate) fn parse(input: parco::PositionedString<'_>) -> ParsingResult<'_, Node> {
+        text::parse(input).map(|text| Node::Text(text)).or(|| {
+            parco::one_matching_part(input, |c| *c == '[').and(|(_c, rest)| {
+                parco::one_matching_part(rest.0, |c| *c == ']')
+                    .map(|_c| Node::Sequence(vec![]))
+                    .or(|| {
+                        sequence_of_nodes::parse(rest.0).and(|(nodes, input)| {
+                            parco::one_matching_part(input.0, |c| *c == ']')
+                                .map(|_c| Node::Sequence(nodes))
+                        })
+                    })
+                    .or(|| parco::Result::Fatal(FatalError::UnclosedBracket { pos: input.pos() }))
+            })
+        })
     }
 }
 
@@ -333,7 +234,7 @@ mod tests {
         assert_eq!(
             parse_sequential_nodes(r#"abc]"#),
             Err(Error::UnexpectedClosingBracket {
-                pos: Position { column: 4, row: 1 }
+                pos: parco::Position { col: 4, row: 1 }
             })
         );
     }
@@ -343,7 +244,7 @@ mod tests {
         assert_eq!(
             parse_sequential_nodes(r#"]"#),
             Err(Error::UnexpectedClosingBracket {
-                pos: Position { column: 1, row: 1 }
+                pos: parco::Position { col: 1, row: 1 }
             })
         );
     }
@@ -354,7 +255,7 @@ mod tests {
             parse_one_node(r#"a["#),
             Ok(Some((
                 text("a"),
-                Rest(("[", Position { row: 1, column: 2 }))
+                parco::Rest(("[", parco::Position { row: 1, col: 2 }))
             )))
         );
     }
@@ -390,7 +291,7 @@ mod tests {
         assert_eq!(
             parse_sequential_nodes(r#"a\b"#),
             Err(Error::UnknownCharacterEscaped {
-                pos: Position { column: 3, row: 1 }
+                pos: parco::Position { col: 3, row: 1 }
             })
         );
     }
@@ -400,7 +301,7 @@ mod tests {
         assert_eq!(
             parse_one_node(r#"["#),
             Err(Error::UnclosedBracket {
-                pos: Position { column: 1, row: 1 }
+                pos: parco::Position { col: 1, row: 1 }
             })
         );
     }
@@ -410,7 +311,7 @@ mod tests {
         assert_eq!(
             parse_sequential_nodes("\n["),
             Err(Error::UnclosedBracket {
-                pos: Position { column: 1, row: 2 }
+                pos: parco::Position { col: 1, row: 2 }
             })
         );
     }
@@ -420,7 +321,7 @@ mod tests {
         assert_eq!(
             parse_sequential_nodes("\n\\a"),
             Err(Error::UnknownCharacterEscaped {
-                pos: Position { column: 2, row: 2 }
+                pos: parco::Position { col: 2, row: 2 }
             })
         );
     }
@@ -430,7 +331,7 @@ mod tests {
         assert_eq!(
             parse_sequential_nodes(r#"a[b"#),
             Err(Error::UnclosedBracket {
-                pos: Position { column: 2, row: 1 }
+                pos: parco::Position { col: 2, row: 1 }
             })
         );
     }
