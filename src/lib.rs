444405d99cda22dab2578188f73b4c90d631cef8
megahomyak <g.megahomyak@gmail.com>
2023-01-16 11:40:47 +0500

    Made string types more general

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,21 @@ use std::collections::HashMap;
 
 use thiserror::Error;
 
-pub fn fields<'a>(iter: impl Iterator<Item = &'a Node>) -> HashMap<&'a String, &'a [Node]> {
+pub fn fields<'a>(iter: impl Iterator<Item = &'a Node>) -> HashMap<&'a str, &'a [Node]> {
     let mut fields = HashMap::new();
     for sequence in sequences(iter) {
         let mut iter = sequence.iter();
         if let Some(Node::Text(text)) = iter.next() {
-            fields.insert(text, iter.as_slice());
+            fields.insert(text.as_str(), iter.as_slice());
         }
     }
     fields
 }
 
-pub fn texts<'a>(iter: impl Iterator<Item = &'a Node>) -> impl Iterator<Item = &'a String> {
+pub fn texts<'a>(iter: impl Iterator<Item = &'a Node>) -> impl Iterator<Item = &'a str> {
     iter.filter_map(|node| {
         if let Node::Text(text) = node {
-            Some(text)
+            Some(text.as_str())
         } else {
             None
         }
