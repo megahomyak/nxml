adbb013548399a8d50cd5bf108e3d24f799f62f1
megahomyak <g.megahomyak@gmail.com>
2023-01-16 10:39:35 +0500

    Added some convenience functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,37 @@
+use std::collections::HashMap;
+
 use thiserror::Error;
+
+pub fn fields<'a>(iter: impl Iterator<Item = &'a Node>) -> HashMap<&'a String, &'a [Node]> {
+    let mut fields = HashMap::new();
+    for sequence in sequences(iter) {
+        let mut iter = sequence.iter();
+        if let Some(Node::Text(text)) = iter.next() {
+            fields.insert(text, iter.as_slice());
+        }
+    }
+    fields
+}
+
+pub fn texts<'a>(iter: impl Iterator<Item = &'a Node>) -> impl Iterator<Item = &'a String> {
+    iter.filter_map(|node| {
+        if let Node::Text(text) = node {
+            Some(text)
+        } else {
+            None
+        }
+    })
+}
+
+pub fn sequences<'a>(iter: impl Iterator<Item = &'a Node>) -> impl Iterator<Item = &'a Vec<Node>> {
+    iter.filter_map(|node| {
+        if let Node::Sequence(sequence) = node {
+            Some(sequence)
+        } else {
+            None
+        }
+    })
+}
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Node {
